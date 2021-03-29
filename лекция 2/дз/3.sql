7dd8ead883b8081da97ad60ee0207cb245aac0c9
sfalll <alexkrasnih0914@gmail.com>
2021-03-29 22:47:40 +0300

    hw2

--- лекция 2/дз/3.sql
+++ лекция 2/дз/3.sql
@@ -1,25 +1,21 @@
 WITH jan_perc (quest_rk, Success) AS (
     SELECT quest_rk, SUM(game_flg) / CAST(COUNT(game_flg) AS float8)
-    FROM
-        game
-        INNER JOIN calendar ON calendar.calendar_dt = game.game_dttm
-    WHERE month_nm = 'January'
+    FROM game
+    WHERE EXTRACT(MONTH FROM game_dttm) = 1
     GROUP BY quest_rk
 ),
 
 feb_perc (quest_rk, Success) AS (
     SELECT quest_rk, SUM(game_flg) / CAST(COUNT(game_flg) AS float8)
-    FROM
-        game
-        INNER JOIN calendar ON calendar.calendar_dt = game.game_dttm
-    WHERE month_nm = 'February'
+    FROM game
+    WHERE EXTRACT(MONTH FROM game_dttm) = 2
     GROUP BY quest_rk
 )
 
-SELECT quest_nm, ABS(jp.Success - fp.Success) AS Разность
+SELECT quest_nm
 FROM
-    feb_perc as fp
+    feb_perc AS fp
     INNER JOIN jan_perc AS jp USING (quest_rk)
     INNER JOIN quest AS q USING (quest_rk)
-ORDER BY 2 DESC, quest_rk DESC
+ORDER BY ABS(jp.Success - fp.Success) DESC, quest_rk DESC
 LIMIT 1;
